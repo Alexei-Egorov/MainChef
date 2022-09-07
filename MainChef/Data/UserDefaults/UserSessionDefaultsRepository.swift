8d6e11761905b2a93e1500f38c7e4f26127bd5a5
Aliaksei Yahorau <ayahorau@lbpro.pl>
2022-09-07 14:10:15 +0200

    Intercomputer commit

---
+++ MainChef/Data/UserDefaults/UserSessionDefaultsRepository.swift
@@ -0,0 +1,9 @@
+//
+//  UserSessionDefaultsRepository.swift
+//  MainChef
+//
+//  Created by Aliaksei Yahorau on 06/09/2022.
+//  Copyright © 2022 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import Foundation
